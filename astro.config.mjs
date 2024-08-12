4d8894a6e20c2f5098d4621cbb3bf3b21b00e188
Yehison Perdomo <yehison93@gmail.com>
2024-08-11 21:06:17 -0400

    "Removed static middleware from astro.config.mjs and updated href in Contact.astro to remove '/public/' prefix"

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,13 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  middleware: [
-    {
-      name: "static",
-      options: {
-        dir: "public",
-        prefix: "/",
-      },
-    },
-  ],
+  // middleware: [
+  //   {
+  //     name: "static",
+  //     options: {
+  //       dir: "public",
+  //       prefix: "/",
+  //     },
+  //   },
+  // ],
 });
