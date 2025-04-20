70f345abde391838d18895fce2444cbdd7e459e2
Yehison Perdomo <yehison93@gmail.com>
2025-04-20 18:25:01 -0400

    Implement code changes to enhance functionality and improve performance

--- src/components/ShuffledBadge.jsx
+++ src/components/ShuffledBadge.jsx
@@ -1,15 +1,21 @@
 import { useMemo } from "react";
 
 const ShuffledBadge = ({ dataSkills }) => {
-  const shuffledSkills = useMemo(
-    () => [...dataSkills].sort(() => Math.random() - 0.5),
-    [dataSkills]
-  );
+  const shuffledSkills = useMemo(() => {
+    const array = [...dataSkills];
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }, [dataSkills]);
   return (
     <div className="container">
       <div className="row p-1 m-auto container-skills-main">
         {shuffledSkills.map((item) => (
-          <div className="col badge container-badges">{item}</div>
+          <div key={item.id || item} className="col badge container-badges">
+            {item.name || item}
+          </div>
         ))}
       </div>
     </div>
